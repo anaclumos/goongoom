a2c623f12861c2bf763c2df2baeb329bad2ad72b
Sunghyun Cho <hey@cho.sh>
2026-01-22 01:38:11 +0900

    fix: remove redundant schema creation from migration script

--- drizzle/0000_faulty_masked_marvel.sql
+++ drizzle/0000_faulty_masked_marvel.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA "goongoom";
---> statement-breakpoint
 CREATE TABLE "goongoom"."answers" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "goongoom"."answers_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"question_id" integer NOT NULL,
